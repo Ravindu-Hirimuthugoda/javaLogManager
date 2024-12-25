8f5ae2a9dc063efb7956d797a62dcd428209d982
RavinduHirimuthugoda <h.ravindu@gtngroup.com>
2024-12-25 11:17:02 +0530

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com"
-version = "1.0-SNAPSHOT"
+version = "1.2-SNAPSHOT"
 
 repositories {
   mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
   patchPluginXml {
     sinceBuild.set("222")
-    untilBuild.set("232.*")
+    untilBuild.set("242.*")
   }
 
   signPlugin {
